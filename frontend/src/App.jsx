68837d70f27222aba8781cb93d41e25ee95d7424
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-07-10 00:27:21 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-amber-300 text-center text-4xl text-bold text-shadow-blue-950 border-4'>
+      hello
+    </div>
+  )
+}
+
+export default App
