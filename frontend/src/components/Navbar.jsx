0ef30df015a647060b2bc911f388c1b2f6173e96
VRAJESH N PANDYA <npandyavrajesh31@gmail.com>
2025-07-13 23:06:55 +0530

    added frontend updates

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProfileInfo from './ProfileInfo';
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
-
+    const [searchQuery, setSearchQuery] = useState("");
     const navigate = useNavigate();
 
     const onLogout = () => {
         navigate("/login");
     };
 
-    return (
+    const handleSearch = () => {
         
+    };
+
+    const onClearSearch = () => {
+        setSearchQuery("");
+    }
+
+    return (
+
         <div className='bg-zinc-950 flex items-center justify-between px-6 py-2 drop-shadow text-white'>
             <h2 className="text-xl font-medium py-2">Notes</h2>
+            <SearchBar value={searchQuery}
+                onChange={({ target }) => {
+                    setSearchQuery(target.value);
+                }}
+                handleSearch={handleSearch}
+                onClearSearch={onClearSearch}
+            />
             <ProfileInfo onLogout={onLogout} />
         </div>
-        
-        
+
+
     )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import NoteCard from '../components/NoteCard';
 
 const Home = () => {
     return (
         <>
             <Navbar />
+            <div className="container mx-auto px-4 py-6">
+                <NoteCard
+                    title="Meeting on 7th April"
+                    date="3rd Apr 2025"
+                    content="Don't forget to prepare slides and update the agenda before the meeting starts" tags="#meeting"
+                    isPinned={true}
+                    onEdit={() => { }}
+                    onDelete={() => { }}
+                    onPinNote={() => { }}
+                />
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
